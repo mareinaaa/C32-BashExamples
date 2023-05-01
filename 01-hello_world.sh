7c593cac618256306949c404d314cb22e29576ce
mareinaaa <mareinaaa@gmail.com>
2023-05-01 09:56:23 -0400

    Hello WORLD

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "Hello" $USER
+
+# exit 
+
+echo "Vive"
+echo "Linux"
+
+# -e permet d'interpreter les retours de ligne et etc (\n,\t,...)
+echo -e "1\n2\n3"
+
+read -p "Quel age avez-vous?" age
+echo $age "! Vous etes jeune"
