464b21d2dc843027c6e698667e5d55ccef2af2c8
mareinaaa <mareinaaa@gmail.com>
2023-05-03 12:28:49 -0400

    AAAA

--- ex8_Lab.sh
+++ ex8_Lab.sh
@@ -1,3 +1,8 @@
 # 8- Calcul simple
 # Le script doit additionner deux nombres entiers. Les deux nombres ne sont pas des parametres du script mais doivent
 # etre demande a  l'utilisateur.
+
+read -p "Nb1: " Nb1
+read -p "Nb2: " Nb2
+let addition="$Nb1+$Nb2"
+echo $addition
